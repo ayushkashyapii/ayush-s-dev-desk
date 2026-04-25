5fdc96daef3fa9d3d79580f56ad24b50b8abc3e7
Ayush KAshyap <kashyap11ayush02@gmail.com>
2026-04-25 19:08:59 +0530

    dary..its gonna be legendary

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    tanstackStart(),
+    react(),
+  ],
+});
